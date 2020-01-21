78e43d80324506c7bb913145673bc5809fe3e37a
Dienul <haqdienul@gmail.com>
2020-01-21 13:58:15 +0700

    Update main-loop.py

--- main-loop.py
+++ main-loop.py
@@ -1,4 +1,34 @@
-#Looping with For 
-myList = ["list one", "list two", "list there"]
-for list in myList :
- print (list)
+# Looping with For
+# myList = ["list one", "list two", "list there"]
+# for list in myList :
+#  print (list)
+
+""" 
+*****
+*****
+*****
+*****
+***** 
+"""
+# string = ''
+# for i in range(5):
+#     # print({"i" : i})
+#     for j in range(5):
+#         string += '*'
+#     print(string)
+#     string = ''
+    
+""" 
+*
+**
+***
+****
+***** 
+"""
+
+string = ''
+for i in range(1,5+1) :
+  for j  in range(i):
+    string += "*"
+  print(string)
+  string = ''
